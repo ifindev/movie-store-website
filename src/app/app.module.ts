158398311e0b376a3aa999b12257d4bb5f9f29cf
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-14 22:16:04 +0700

    Add signup-page view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './views/landing-page/landing-page.component';
 import { LoginPageComponent } from './views/login-page/login-page.component';
+import { SignupPageComponent } from './views/signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
