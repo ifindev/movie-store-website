8bd883c5a627b8f104a5bbf4579f36107cab3108
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-14 22:15:15 +0700

    Add login view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { LoginPageComponent } from './views/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
