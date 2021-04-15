f5db8b02a371ff9c9245ab9710229b9f12d4a1ca
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-16 06:43:21 +0700

    Add router module to signup components

--- src/app/layout/signup-mobile/signup-mobile.component.ts
+++ src/app/layout/signup-mobile/signup-mobile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
