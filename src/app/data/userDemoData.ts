edfa870ccbb18b2386b7c3017eb035b084fb2879
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-16 03:34:03 +0700

    Add user data for demo

---
+++ src/app/data/userDemoData.ts
@@ -0,0 +1,30 @@
+export const usersData = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    password: 'kulaslight',
+  },
+  {
+    id: 2,
+    name: 'Patrick Star',
+    username: 'pstar',
+    email: 'pstar@bikini.bottom',
+    password: 'awesomepat',
+  },
+  {
+    id: 3,
+    name: 'Spongebob Squarepants',
+    username: 'spongebob',
+    email: 'spongebob@bikini.bottom',
+    password: 'ilovegarry',
+  },
+  {
+    id: 4,
+    name: 'Squidy Squidward',
+    username: 'squidward',
+    email: 'squidward@bikini.bottom',
+    password: 'iloveclarinet',
+  },
+];
