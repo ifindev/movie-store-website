f4e02e4dbc1f5627275b85f93fa85ad342217d26
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-16 03:56:43 +0700

    Import auth service to login page

--- src/app/views/login-page/login-page.component.ts
+++ src/app/views/login-page/login-page.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
