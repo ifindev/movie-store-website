7200eae0cfb97272fa35e9c4d43c04de42389a29
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-15 09:36:52 +0700

    Add event emitter for signupEvent on signup mobile

--- src/app/layout/signup-mobile/signup-mobile.component.ts
+++ src/app/layout/signup-mobile/signup-mobile.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-mobile',
   templateUrl: './signup-mobile.component.html',
-  styleUrls: ['./signup-mobile.component.css']
+  styleUrls: ['./signup-mobile.component.css'],
 })
 export class SignupMobileComponent implements OnInit {
+  @Output() signupEvent: EventEmitter<object> = new EventEmitter();
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
