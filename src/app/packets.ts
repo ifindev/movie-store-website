ee6e65608d73455a6dc8527569af0a62b915978b
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-16 03:22:10 +0700

    Add packets data

---
+++ src/app/packets.ts
@@ -0,0 +1,13 @@
+export const packets = [
+  {
+    id: 1,
+    name: 'Beli Film Aja',
+    description:
+      'Beli film kapanpun saat kamu mau nonton tidak perlu langganan',
+  },
+  {
+    id: 2,
+    name: 'Membership',
+    description: 'Langganan nonton film. Nonton sepuasnya, bayar tiap bulan.',
+  },
+];
