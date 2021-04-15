dc8df429baf658d47b58b16077ea932d254b8130
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-16 03:54:00 +0700

    Add autLogin function to auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,5 +6,29 @@ import { usersData } from '../data/userDemoData';
   providedIn: 'root',
 })
 export class AuthService {
+  usersData = usersData;
+
+  /* Login Auth */
+  authLogin(userLogin: {
+    email: string;
+    password: string;
+  }): { status: number; msg: string } {
+    const found = usersData.find(
+      (user) =>
+        user.email === userLogin.email && user.password === userLogin.password
+    );
+    if (found) {
+      return {
+        status: 202,
+        msg: 'login success',
+      };
+    } else {
+      return {
+        status: 500,
+        msg: 'user not found',
+      };
+    }
+  }
+
   constructor() {}
 }
