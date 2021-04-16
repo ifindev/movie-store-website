d63db00a8394b41c71b30a79f58c6230ca70cd4b
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-16 13:35:13 +0700

    Add cart service in navbar-bfa component

--- src/app/layout/navbar-bfa/navbar-bfa.component.ts
+++ src/app/layout/navbar-bfa/navbar-bfa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-navbar-bfa',
@@ -6,9 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./navbar-bfa.component.css'],
 })
 export class NavbarBfaComponent implements OnInit {
-  @Input() moviesInCart: number;
+  moviesInCart: number;
 
-  constructor() {}
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {}
 }
