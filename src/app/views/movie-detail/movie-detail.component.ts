ef8edae8d3d959249b001d13880c90912a1d140f
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-16 12:53:23 +0700

    Add ActivatedRoute to movie-detail.component.ts

--- src/app/views/movie-detail/movie-detail.component.ts
+++ src/app/views/movie-detail/movie-detail.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.css']
+  styleUrls: ['./movie-detail.component.css'],
 })
 export class MovieDetailComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
