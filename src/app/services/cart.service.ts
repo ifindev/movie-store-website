16931ce601f0037d6545bbe67199e7a943a43474
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-16 13:22:58 +0700

    add methods to cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  movieCart = [];
 
-  constructor() { }
+  addToCart(movie) {
+    this.movieCart.push(movie);
+  }
+
+  removeFromCart(movie) {
+    this.movieCart = this.movieCart.filter((item) => item.id !== movie.id);
+    console.log('removed from cart: ', movie);
+    console.log(this.movieCart);
+  }
+
+  getMoviesInCart() {
+    return this.movieCart;
+  }
+
+  constructor() {}
 }
