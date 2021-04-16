5fe71cbf6ade606c7a22f0013ccfdb733ebe99f8
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-16 11:48:38 +0700

    Import movies data into movie-dashboard

--- src/app/views/movie-dashboard/movie-dashboard.component.ts
+++ src/app/views/movie-dashboard/movie-dashboard.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { movies } from '../../data/movies';
 
 @Component({
   selector: 'app-movie-dashboard',
   templateUrl: './movie-dashboard.component.html',
-  styleUrls: ['./movie-dashboard.component.css']
+  styleUrls: ['./movie-dashboard.component.css'],
 })
 export class MovieDashboardComponent implements OnInit {
+  movies = movies;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
